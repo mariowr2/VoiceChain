ed88b16c7296d46234eb905dca2d529f8cf7b2a4
Miguel Valarao <miguel.valarao@gmail.com>
2019-09-14 08:41:52 -0400

    Set up material ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 function App() {
+  const useStyles = makeStyles(theme => ({
+    button: {
+      margin: theme.spacing(1),
+    },
+    input: {
+      display: 'none',
+    },
+  }));
+  const classes = useStyles();
+
   return (
     <div className="App">
-      Boop
+      <Typography>
+        This is text
+      </Typography>
+      <Button variant="contained" color="primary" className={classes.button}>
+        This is a button
+      </Button>
     </div>
   );
 }
