d012a285f09dd59bfa1af30f3ab72379631a25d3
Miguel Valarao <miguel.valarao@gmail.com>
2019-09-14 15:51:56 -0400

    Delete unnecessary imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import { Router, Route, Switch } from 'react-router-dom';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 
 import Navbar from './Navbar';
 import Main from './pages/MainPage';
